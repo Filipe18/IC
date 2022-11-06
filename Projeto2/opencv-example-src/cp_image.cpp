a5b2891cc8b706def587afee302158b35b98d30f
Ricardo-Antunes0 <ricardofantunes@ua.pt>
2022-11-06 11:03:33 +0000

    Projeto 2

---
+++ Projeto2/opencv-example-src/cp_image.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {	
+
+    if (argc < 3) {	
+        cerr << "Usage: " << argv[0] << " <input file> <output file>\n" ;
+        return 1;
+    }
+
+ 
+    return 0;
+}
