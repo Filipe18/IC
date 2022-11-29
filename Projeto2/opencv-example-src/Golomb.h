8755affba6439d3e650c94cd14edc40da0baa5c5
Miguel Tavares <miguel.tavares07@ua.pt>
2022-11-29 14:18:07 +0000

    Golomb complete

--- Projeto2/opencv-example-src/Golomb.h
+++ Projeto2/opencv-example-src/Golomb.h
@@ -16,7 +16,8 @@ class Golomb {
             int q, r;
             string bit_string, aux, temp; 
 
-            // swith signed integers we need to perform some changes:
+            // with signed integers we need to perform some changes:
+            
             // When number is greater than or equal to zero, the number is mapped to the m-th even number (number*2)
             if ( number >= 0){
                 number *= 2;
@@ -32,17 +33,17 @@ class Golomb {
                 bit_string += '1';
             }
             bit_string += '0';
-            int b = floor(log2(m));
             // check if m is a power of 2
-            if ((log2(m) - b) != 0) {
+            if (ceil(log2(m)) != floor(log2(m))) {
+                int b = ceil(log2(m));
                 int n_bits;
 
-                if (r < pow(2, b+1) - m){
-                    n_bits = b;
+                if (r < pow(2, b) - m){
+                    n_bits = b-1;
                 }
                 else{
-                    r += pow(2,b+1) - m;
-                    n_bits = b + 1;
+                    r += pow(2,b) - m;
+                    n_bits = b;
                 }
                 aux = bitset<64>(r).to_string();
                 for (int j = (63 - n_bits)+1; j <= 63; j++){
@@ -52,7 +53,7 @@ class Golomb {
             }
             else{
                 aux = bitset<64>(r).to_string();
-                for (int j = (63 - b)+1; j <= 63; j++){
+                for (int j = (63 - log2(m))+1; j <= 63; j++){
                     temp += aux[j];
                 }
                 bit_string += temp;
@@ -66,10 +67,10 @@ class Golomb {
             int q = 0;
             string in_q;
 
-            // Descobrir a posição onde está o primeiro '0'(delimitador)
+            // Descobrir a posição onde está o primeiro '1'(delimitador)
             // Se tiver na primeira, significa que não há quociente. Se nao tiver na primeira, o quociente acaba na posição anterior à do delimitador
-            // Exemplo : '11101110' -> quociente = 111
-            int sep = (int) bit_string.find("0");
+            // Exemplo : '00011110' -> quociente = 000
+            int sep = (int) bit_string.find("1");
 
             for(int i = 0; i < sep; i++){
                 q++;        // number of '1' before the first '0'
@@ -79,10 +80,11 @@ class Golomb {
             int b = floor(log2(m));
             r = stoi(bit_string.substr(sep+1,b), 0, 2); // interpret next b bits as a binary number r'
             if(r >= pow(2, b+1) - m){
-                r = stoi(bit_string.substr(sep+1,b+1), 0, 2); // // interpret next b+1 bits as a binary number r'
+                r = stoi(bit_string.substr(sep+1,b+1), 0, 2); // interpret next b+1 bits as a binary number r'
                 r = r - pow(2, b+1) + m;
             }
             int number = q*m + r;
+            
             if( number % 2 == 0){
                 return number/2;
             }
@@ -90,7 +92,7 @@ class Golomb {
                 return (-number - 1)/2;
             }
         }
-
+        
 };
 
 #endif
--- Projeto2/opencv-example-src/testGolomb.cpp
+++ Projeto2/opencv-example-src/testGolomb.cpp
@@ -5,10 +5,9 @@ using namespace std;
 int main() {
 
     Golomb g;
-    for(int i = -5; i <= 5; i++){
-        string bit_string = g.encode(i,5);
-        cout << bit_string << endl;
-        cout << g.decode(bit_string,5) << endl;
+    
+    for(int i = -8; i <= 7; i++){
+        cout << i << " -> " << g.encode(i,1) << endl;
     }
     
 }  
